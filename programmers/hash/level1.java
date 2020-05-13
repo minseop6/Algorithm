a86913653c74a6a8106147889e533c53acf24666
gim34 <gim34@naver.com>
2020-05-13 21:00:29 +0900

    2020-05-13

---
+++ programmers/hash/level1.java
@@ -0,0 +1,26 @@
+package hash;
+
+import java.util.HashMap;
+
+class level1 {
+    public String solution(String[] participant, String[] completion) {
+        
+        String answer = "";
+        
+		HashMap<String, Integer> hash = new HashMap<>();
+		
+		for(String element : participant) {
+			hash.put(element, hash.getOrDefault(element, 0) + 1);
+		}
+		for(String element : completion) {
+			hash.put(element, hash.get(element) - 1);
+		}
+		for(String key : hash.keySet()) {
+			if(hash.get(key) != 0) {
+				answer += key;
+			}
+		}
+        
+        return answer;
+    }
+}
