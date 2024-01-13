00258323015cdd386e26412977e0917be9e4a4ce
minseop6 <gim34@naver.com>
2024-01-13 15:42:05 +0900

    큰수만들기

--- java/programmers/Runner.java
+++ java/programmers/Runner.java
@@ -2,9 +2,15 @@ import java.util.Arrays;
 
 public class Runner {
   public static void main(String[] args) {
-    var result = new 더맵게().solution(new int[] { 1, 2, 3, 9, 10, 12 }, 7);
+    var result = new 큰수만들기().solution("1924", 2);
     System.out.println("=======================result");
     System.out.println(result);
+    var result2 = new 큰수만들기().solution("1231234", 3);
+    System.out.println("=======================result2");
+    System.out.println(result2);
+    var result3 = new 큰수만들기().solution("4177252841", 4);
+    System.out.println("=======================result3");
+    System.out.println(result3);
 //    Arrays.stream(result).forEach(System.out::println);
   }
 }
---
+++ java/programmers/큰수만들기.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class 큰수만들기 {
+    public String solution(String number, int k) {
+
+        var numbers = Arrays.stream(number.split("")).mapToInt(Integer::valueOf).toArray();
+        var left = 0;
+        StringBuilder answer = new StringBuilder();
+        for (var sequence = 0; sequence < numbers.length - k; sequence++)  {
+            var largestNumber = 0;
+            for (var i = left; i <= numbers.length - (numbers.length - k - sequence); i++) {
+                if (numbers[i] > largestNumber) {
+                    left = i + 1;
+                    largestNumber = numbers[i];
+                }
+            }
+            answer.append(largestNumber);
+        }
+
+        return answer.toString();
+    }
+}
