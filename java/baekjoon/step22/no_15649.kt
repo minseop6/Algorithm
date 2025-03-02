dd161b9c8413892e78248b1d3279d0f14782acd9
minseop6 <gim34@naver.com>
2025-03-02 16:48:36 +0900

    no_15649

---
+++ java/baekjoon/step22/no_15649.kt
@@ -0,0 +1,33 @@
+package step22
+
+import java.util.*
+
+fun main() = with(Scanner(System.`in`)) {
+    val number = nextInt()
+    val length = nextInt()
+    val numbers = (1..number).toList()
+
+    val backTracker = NumberBackTracker(numbers, length)
+
+    val result = numbers.flatMap { backTracker.search(setOf(it)) }
+
+    result.forEach { println(it.joinToString(" ")) }
+}
+
+data class NumberBackTracker(
+    private val numbers: List<Int>,
+    private val maxLength: Int,
+) {
+    fun search(currentValues: Set<Int>): List<List<Int>> {
+        if (currentValues.size == maxLength) {
+            return listOf(currentValues.toList())
+        }
+
+        return numbers.mapNotNull { number ->
+            if (currentValues.contains(number)) {
+                return@mapNotNull null
+            }
+            search(currentValues + number)
+        }.flatten()
+    }
+}
